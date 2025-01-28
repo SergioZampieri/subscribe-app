ef8edecab3e7367c7f5368e71c1afed79f678e8e
Sergio <gersio.zampieri@gmail.com>
2025-01-28 17:29:33 -0300

    nextjs.yml fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/nextjsmarketplace" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/sergiozampieri" : "",
   images: {
     unoptimized: true,
   },
